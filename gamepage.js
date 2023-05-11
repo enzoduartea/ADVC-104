2ba1b65728483de1ea4358bf15b5ba41a1fd778e
enzoduartea <122171164+enzoduartea@users.noreply.github.com>
2023-05-11 20:21:49 -0300

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,12 @@
+// geração do valor aleatório
+var y = Math.floor(Math.random() * 10 + 1);
+// contagem de tentativas
+// até acerto    
+  
+// função para o número enviado pelo usuário
+var x  = document.getElementById("guessField").value;
+
+   if(x == y)
+   {
+
+   }
